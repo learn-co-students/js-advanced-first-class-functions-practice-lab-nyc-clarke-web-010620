47b863d28e5e7ea8282ba9a2fa89f6556a23400a
github username <github email address>
2020-02-18 17:27:04 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Code your solution in this file!
+const logDriverNames = function(array){for (const o of array){console.log(o.name)}}
+
+const logDriversByHometown = function(array, location){for (const o of array){if (o.hometown === location){console.log(o.name)}}}
+
+const driversByRevenue = function(array){
+    let new_arr = [...array]
+    return new_arr.sort(function(a,b){return a.revenue - b.revenue})
+}
+const driversByName = function(array){
+    let new_arr = [...array]
+    return new_arr.sort(function(a,b){return a.name.localeCompare(b.name)})
+}
+const totalRevenue = function(array){
+    let tr = 0
+    for (const o of array){tr += o.revenue}
+    return tr
+}
+const averageRevenue = function(array){
+    return totalRevenue(array) / array.length
+}
