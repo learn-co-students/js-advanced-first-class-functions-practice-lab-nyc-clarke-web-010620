da9a2f2176a38bb204d2c2f35a4957b079a24017
jhuang429 <jhuang9240@gmail.com>
2020-02-18 21:09:23 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
 // Code your solution in this file!
+function logDriverNames(arr) {
+    arr.forEach(element => {
+        console.log(element.name)
+    });
+}
+
+function logDriversByHometown(arr, loc) {
+    let new_arr = arr.filter( driver => driver.hometown === loc)
+    logDriverNames(new_arr)
+};
+
+function driversByRevenue(arr) {
+    let newarr = [...arr]
+    return newarr.sort((a, b) => a.revenue - b.revenue)
+}
+
+function driversByName(arr) {
+    let newarr = [...arr]
+    return newarr.sort((a, b) => a.name.localeCompare(b.name))
+}
+
+function totalRevenue(arr) {
+    return arr.reduce((a, b) => {
+        return a + b.revenue
+    }, 0)
+}
+
+function averageRevenue(arr) {
+    return totalRevenue(arr)/arr.length
+}
